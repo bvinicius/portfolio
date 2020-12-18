9d94d8dcf268fe8c6d04dd54da5965bb0ba9ef6c
vbazanella <vinicius.bazanella1@gmail.com>
2020-12-17 22:29:57 -0300

    implements about me and starts implementation of skills section

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
+'use strict'
+
 import { run } from './src/utils/typewrite-effect/typewrite-effect.js'
 
 window.onload = () => {
+    const height = window.outerHeight
+    const element = document.getElementById('welcome')
+    element.style.height = height - 50 + "px"
+
     run('name', ' Vinicius')
 }
